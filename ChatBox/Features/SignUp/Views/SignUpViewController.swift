1e4bc2472b90ac8fb8dcd5e547fc80da31842885
Israa <isramuhammad613@gmail.com>
2024-11-27 07:06:15 -0800

    finish signUp screen UI

--- ChatBox/Features/SignUp/Views/SignUpViewController.swift
+++ ChatBox/Features/SignUp/Views/SignUpViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var confirmedPassword: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var signUpLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+    
+    let signUpText = "Sign up with "
+    let EmailText = "Email"
+    
+    let highlightedAttributes : [NSAttributedString.Key:Any] = [
+        .font:UIFont.boldSystemFont(ofSize:24),
+        .underlineStyle:NSUnderlineStyle.thick.rawValue,
+        .underlineColor: UIColor.green ,
+    ]
+    
+    let normalAttributes : [NSAttributedString.Key:Any] = [
+        .font: UIFont.boldSystemFont(ofSize: 24)
+    ]
+    
+    let highlightedString = NSMutableAttributedString(string: EmailText, attributes: highlightedAttributes)
+    
+    let normalString = NSMutableAttributedString(string: signUpText, attributes: normalAttributes)
+    
+    normalString.append(highlightedString)
+    
+    signUpLabel.attributedText = normalString
     }
-
-
+    
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
